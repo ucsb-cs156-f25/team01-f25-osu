d5fd9fd3c342317229ea962a9b7d0ddca2cbda94
Summit Haque <summit.haque@gmail.com>
2025-11-05 16:34:55 -0800

    UCSBDiningCommonsMenuItemsRepository.java file added in the repositories part

---
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsMenuItemsRepository.java
@@ -0,0 +1,10 @@
+package edu.ucsb.cs156.example.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import edu.ucsb.cs156.example.entities.UCSBDiningCommonsMenuItems;
+
+@Repository
+public interface UCSBDiningCommonsMenuItemsRepository extends CrudRepository<UCSBDiningCommonsMenuItems, Long> {
+	
+}

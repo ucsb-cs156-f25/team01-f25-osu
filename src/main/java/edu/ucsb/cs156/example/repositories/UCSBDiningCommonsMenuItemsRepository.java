cc87311259cf83856df5d6323e7c9eff5aba2556
Summit Haque <summit.haque@gmail.com>
2025-11-07 11:02:22 -0800

    UCSBDiningCommonsMenuItemsRepository.java formatting error fixed

--- src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsMenuItemsRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsMenuItemsRepository.java
@@ -1,10 +1,9 @@
 package edu.ucsb.cs156.example.repositories;
 
+import edu.ucsb.cs156.example.entities.UCSBDiningCommonsMenuItems;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import edu.ucsb.cs156.example.entities.UCSBDiningCommonsMenuItems;
 
 @Repository
-public interface UCSBDiningCommonsMenuItemsRepository extends CrudRepository<UCSBDiningCommonsMenuItems, Long> {
-	
-}
+public interface UCSBDiningCommonsMenuItemsRepository
+    extends CrudRepository<UCSBDiningCommonsMenuItems, Long> {}
